71baf865157c0de1dc3d81dccb797f3b4716ae98
Elodie Lachoua <elodie.lachoua@gmail.com>
2017-08-21 17:46:17 +0200

    rooms controller + routes

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,27 @@
 class RoomsController < ApplicationController
 skip_before_action :authentificate!, only: :show
+  def show
+    @room = Room.find(params[:id])
+  end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to room_path(@room)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:title, :decription, :custom_message, :photo)
+
+  end
 
 end
