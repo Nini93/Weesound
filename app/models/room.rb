2c6d55c8794f583ec312a86283e308627d0d9e9e
Elodie Lachoua <elodie.lachoua@gmail.com>
2017-08-21 17:22:44 +0200

    add photo to user's model and room's model

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,5 @@ class Room < ApplicationRecord
   belongs_to :user
   has_many :tracks, dependent: :destroy
   validates :title, :description, :custom_message, presence: true
+  has_attachment :photo
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :rooms, dependent: :destroy
   validates :username, presence: true
+  has_attachment :photo
 end
