48ac26d6baa676a1b1c8c4b2eb01af69afea95a3
Elodie Lachoua <elodie.lachoua@gmail.com>
2017-08-22 11:50:30 +0200

    seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "destrucution des rooms"
+Room.destroy_all
+puts "création user 1"
+user_1 = User.create!(username: "Elo", email: "elodie@gmail.com", password: "123456")
+puts "Création room"
+room_1 = Room.new(title: "Dream Team Room", description: "Best Room ever", custom_message: "Love you all guys", user_id: user_1.id)
+puts "save room"
+room_1.save
+puts "done"
+
+
