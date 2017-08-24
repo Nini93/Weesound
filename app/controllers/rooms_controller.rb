fdbebb76f02478571e3c81d0f83c98fdc18b0ff2
Fabienne Bremond <fabienne.bremond@gmail.com>
2017-08-24 12:31:53 +0200

    hh

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -8,7 +8,6 @@ skip_before_action :authenticate_user!, only: :show
   def new
     @room = Room.new
     @track = Track.new
-
   end
 
   def create
@@ -23,15 +22,20 @@ skip_before_action :authenticate_user!, only: :show
   def current_track
     @room = Room.find(params[:id])
     @elapsed_time = Time.now.utc.to_i - @room.created_at.to_i
+    @my_track = ""
     @room.tracks.each do |t|
       if @elapsed_time - t.duration > 0
         @elapsed_time -= t.duration
-        puts "#{elapsed_time}"
+        puts "#{elapsed_time}, #{t.duration}"
+        p "#{@room.tracks}"
       elsif @elapsed_time - t.duration <= 0
-        @current_track = t.youtubeid
+        @my_track = t.youtubeid
+        break
+        p "#{@room.tracks}"
       end
     end
   end
+
 
 
   private
@@ -40,4 +44,14 @@ skip_before_action :authenticate_user!, only: :show
     params.require(:room).permit(:title, :decription, :custom_message, :photo)
   end
 
+end
+
+index = 0
+for index in 0...room.tracks.size
+  if elapsed_time - room.tracks[index].duration > 0
+    elapsed_time -= room.tracks[index].duration
+  elsif elapsed_time - room.tracks[index].duration <= 0
+    my_track = room.tracks[index].youtubeid
+    break
+  end
 end
