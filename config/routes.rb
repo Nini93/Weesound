608d3a3610d10b7410c4bd168221bac27ededde2
Elodie Lachoua <elodie.lachoua@gmail.com>
2017-08-25 15:28:58 +0200

    config redis cloud

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
   mount Attachinary::Engine => "/attachinary"
+  mount ActionCable.server => "/cable"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :rooms, only: [:new, :create, :show, :update] do
